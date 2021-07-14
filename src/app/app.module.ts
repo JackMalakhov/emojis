335159aa79a9ce3244ca79c9017c4945204bb7a9
Jack malakhov <jack.malakhov@gmail.com>
2021-07-14 11:16:39 +0800

    favicon changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -10,6 +9,8 @@ import { EmojiComponent } from './emoji/emoji.component';
 import { EmojiSearchpipe } from './shared/emoji-search.pipe';
 import { FavouriteComponent } from './favourite/favourite.component';
 import { DeletedComponent } from './deleted/deleted.component';
+
+
 
 @NgModule({
   declarations: [
